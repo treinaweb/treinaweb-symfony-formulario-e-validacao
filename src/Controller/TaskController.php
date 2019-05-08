80275957a99cfe225baaa9531bc01794142e149f
Elton Fonseca <elton869@gmail.com>
2019-05-08 11:21:09 -0300

    projeto inicial do curso

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TaskRepository;
+
+/**
+ * @Route("/task")
+ */
+class TaskController extends AbstractController
+{
+    /**
+     * Lista tarefas do banco de dados
+     * @Route("/", name="tasks_index", methods="GET")
+     *
+     * @return Response
+     */
+    public function index(TaskRepository $repository): Response
+    {
+        return $this->render("tasks/index.html.twig", [
+            "tasks" => $repository->findAll()
+        ]);
+    }
+
+    /**
+     * Cria uma nova tarefa no banco de dados
+     * @Route("/new", name="tasks_new", methods={"GET", "POST"})
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $isTokenValid = $this->isCsrfTokenValid('cadastro_tarefas', $request->request->get('_token'));
+
+        if ($request->isMethod("POST") && $isTokenValid) {
+            $task = new Task;
+            $task->setTitle($request->request->get("title"));
+            $task->setDescription($request->request->get("description"));
+            $task->setScheduling(new \DateTime());
+    
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+    
+            return $this->redirectToRoute("tasks_show", ["id" => $task->getId()]);
+        }
+
+        return $this->render("tasks/new.html.twig");
+    }
+
+     /**
+     * Mostra uma tarefa especifica
+     * @Route("/{id}", name="tasks_show", methods="GET")
+     *
+     * @param Task $task
+     * @return Response
+     */
+    public function show(Task $task): Response
+    {
+        return $this->render("tasks/show.html.twig", [
+            "task" => $task
+        ]);
+    }
+
+
+    /**
+     * Edita uma tarefa no banco de dados
+     * @Route("/{id}/edit", name="tasks_edit", methods={"GET", "POST"})
+     *
+     * @param Request $request
+     * @param Task $task
+     * @return Response
+     */
+    public function edit(Request $request, Task $task): Response
+    {
+        $isTokenValid = $this->isCsrfTokenValid('cadastro_tarefas', $request->request->get('_token'));
+
+        if ($request->isMethod("POST") && $isTokenValid) {
+            $task->setTitle($request->request->get("title"));
+            $task->setDescription($request->request->get("description"));
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute("tasks_show", ["id" => $task->getId()]);
+        }
+
+        return $this->render("tasks/edit.html.twig", [
+            "task" => $task
+        ]);
+    }
+
+    /**
+     * Apaga uma tarefa no banco de dados
+     * @Route("/{id}", name="tasks_delete", methods="DELETE")
+     *
+     * @param Task $task
+     * @return Response
+     */
+    public function delete(Task $task, Request $request): Response
+    {
+        $isTokenValid = $this->isCsrfTokenValid('deletar_tarefa', $request->request->get('_token'));
+
+        if ($isTokenValid) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($task);
+            $entityManager->flush();
+        }
+        
+        return $this->redirectToRoute("tasks_index");
+    }
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $Scheduling;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getScheduling(): ?\DateTimeInterface
+    {
+        return $this->Scheduling;
+    }
+
+    public function setScheduling(\DateTimeInterface $Scheduling): self
+    {
+        $this->Scheduling = $Scheduling;
+
+        return $this;
+    }
+}
