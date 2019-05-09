f75e2607e9af102d8587c1b30a96964ff3f01be7
Elton Fonseca <elton869@gmail.com>
2019-05-09 17:24:11 -0300

    07 - Definindo entidade padrão para form

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -37,9 +37,7 @@ class TaskController extends AbstractController
      */
     public function new(Request $request): Response
     {
-        $task = new Task;
-
-        $form = $this->createForm(TaskType::class, $task);
+        $form = $this->createForm(TaskType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -7,15 +7,37 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Task;
 
 class TaskType extends AbstractType {
 
+    /**
+     * Define campos do formulário
+     *
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     * @return void
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title', TextType::class)
             ->add('description', TextareaType::class)
             ->add('Criar', SubmitType::class);
+    }
+
+    /**
+     * Define configurações do formulário
+     *
+     * @param OptionsResolver $resolver
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class
+        ]);
     }
 
 }
